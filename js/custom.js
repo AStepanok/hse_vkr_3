0192fd241299ec9f9d521a6c9f616fd0e07304e8
Антон Степанок <antonstepanok@MacBook-Pro-Anton.local>
2019-05-18 10:37:17 +0300

    feat(*): routes

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,7 @@ const Y_geoObjects = {};
 let geoObjectsCounter = 0;
 const addressInput = $('#address-input');
 const addressesList = $('.list');
+const routeButton = $('#route');
 
 ymaps.ready(init);
 
@@ -52,6 +53,23 @@ addressesList.on('click', '.geoObject', function () {
    Y_map.geoObjects.remove(Y_geoObjects[id]);
    Y_geoObjects[id] = undefined;
    $(this).remove();
+});
+
+routeButton.on('click', function () {
+    const routedList = Object.keys(Y_geoObjects).map((key) => Y_geoObjects[key])
+    var multiRoute = new ymaps.multiRouter.MultiRoute({
+        // Описание опорных точек мультимаршрута.
+        referencePoints: routedList,
+        // Параметры маршрутизации.
+        params: {
+            // Ограничение на максимальное количество маршрутов, возвращаемое маршрутизатором.
+            results: 20
+        }
+    }, {
+        // Автоматически устанавливать границы карты так, чтобы маршрут был виден целиком.
+        boundsAutoApply: true
+    });
+    Y_map.geoObjects.add(multiRoute);
 });
 
 
